8bf3f7eab512d39d43f5432770c31f2ad8c02ba5
Chris Boyle <cthboyle6@gmail.com>
2022-01-21 21:05:30 -0500

    preliminary header, bookshelf, and book layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Bookshelf from './components/Bookshelf/Bookshelf';
 
 function App() {
   return (
     <div className="App">
       <h1>Bookshelf</h1>
+      <hr></hr>
       {/* Search Box */}
-      {/* Bookshelf */}
+      <Bookshelf />
     </div>
   );
 }
