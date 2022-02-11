bb11c442545885f78419d184c455fb7cdc6eaac5
Chris Boyle <cthboyle6@gmail.com>
2022-02-11 15:38:41 -0500

    format openBook layout, save and edit form based on Locked/Unlocked state

--- src/components/Bookshelf/OpenBook.js
+++ src/components/Bookshelf/OpenBook.js
@@ -1,49 +1,66 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 
-import Modal from '../UI/Modal';
-import Card from '../UI/Card';
+import Modal from "../UI/Modal";
+import Card from "../UI/Card";
 
-import classes from './OpenBook.module.css'
-import Button from '../UI/Button';
+import classes from "./OpenBook.module.css";
+import Button from "../UI/Button";
 
 const OpenBook = (props) => {
-  const ratingValue = document.getElementById('rating-value');
+  const [formLocked, setFormLocked] = useState(true);
+  const ratingValue = useRef();
 
   const closeBookHandler = () => {
-    props.onCloseBook()
+    props.onCloseBook();
   };
 
   const changeRatingHandler = (event) => {
-    ratingValue.innerHTML = event.target.value;
+    const rating = event.target.value;
+    ratingValue.current.innerHTML = rating;
+  };
+
+  const clickSaveHandler = (event) => {
+    event.preventDefault();
+    setFormLocked(prevState => !prevState);
   };
 
   return (
     <Modal onCloseModal={closeBookHandler}>
       <Card className={classes.result}>
-        <div className={classes.left} bookid={props.openBook.id}>
+        <h2>{props.openBook.title}</h2>
+        <div className={classes.top} bookid={props.openBook.id}>
           <img
             src={props.openBook.bookCover}
             alt={`book cover for ${props.openBook.title}`}
           />
+          <div>
+            <p>{props.openBook.description}</p>
+            <div>Page Count: {props.openBook.pageCount}</div>
+          </div>
         </div>
-        <div className={classes.right}>
-          <h2>{props.openBook.title}</h2>
-          <p>{props.openBook.description}</p>
-          <div>Page Count: {props.openBook.pageCount}</div>
-          <form>
-            <div className={classes['form-controls']}>
-              <label htmlFor='date-finished'>Date Finished: </label>
-              <input type="date" id='date-finished'/>
-              <label htmlFor='rating'>Rating: <span id='rating-value'></span></label>
-              <input type='range' id='rating' min='0.5' max='5' step='0.5' onChange={changeRatingHandler}/>
-              <label htmlFor='shared-with'>Shared With: </label>
-              <input type='text' id='shared-with'/>
-            </div>
-            <div className='form-actions'>
-              <Button type='submit'>Save</Button>
-            </div>
-          </form>
-        </div>
+        <form className={classes.bottom}>
+          <div className={classes["form-controls"]}>
+            <label htmlFor="date-finished">Date Finished: </label>
+            <input type="date" id="date-finished" disabled={formLocked} />
+            <label htmlFor="rating">
+              Rating: <span ref={ratingValue}></span>
+            </label>
+            <input
+              type="range"
+              id="rating"
+              min="0.5"
+              max="5"
+              step="0.5"
+              onChange={changeRatingHandler}
+              disabled={formLocked}
+            />
+            <label htmlFor="shared-with">Shared With: </label>
+            <input type="text" id="shared-with" disabled={formLocked} />
+          </div>
+          <div className="form-actions">
+            <Button onClick={clickSaveHandler}>{formLocked ? 'Edit' : 'Save'}</Button>
+          </div>
+        </form>
       </Card>
     </Modal>
   );
